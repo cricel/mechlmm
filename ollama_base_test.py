568e99dd783d52969a221c485b43545112be19a7
Xiangxu Lin <cricel.design@gmail.com>
2024-05-26 22:20:55 -0400

    add simple ollama llm test

---
+++ ollama_base_test.py
@@ -0,0 +1,25 @@
+from ollama import Client
+client = Client(host='http://131.123.41.132:11434')
+
+print ("------ Text LLM ------")
+res = client.chat(model='llama3', messages=[
+  {
+    'role': 'user',
+    'content': 'Hi',
+  },
+])
+print(res['message']['content'])
+
+print ("------ Image LLM ------")
+res = client.chat(
+	model="llava",
+	messages=[
+		{
+			'role': 'user',
+			'content': 'give me a one sentence description of what happen in this image:',
+			'images': ['./messy_room.jpg']
+		}
+	]
+)
+
+print(res['message']['content'])
