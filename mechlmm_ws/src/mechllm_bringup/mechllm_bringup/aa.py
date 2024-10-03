019601e6a002db541e640d9f0946b3d66af30312
cricel <cricel.design@gmail.com>
2024-10-03 17:08:45 -0400

    test code

--- mechlmm_ws/src/mechllm_bringup/mechllm_bringup/aa.py
+++ mechlmm_ws/src/mechllm_bringup/mechllm_bringup/aa.py
@@ -1,7 +1,20 @@
-print("sas")
-a = 3
+import time
 
-def add():
-    global a
-    a = a + 2
-    return a
+# generic ros libraries
+import rclpy
+from rclpy.logging import get_logger
+
+# moveit python library
+from moveit.core.robot_state import RobotState
+from moveit.planning import (
+    MoveItPy,
+    MultiPipelinePlanRequestParameters,
+)
+
+rclpy.init()
+logger = rclpy.logging.get_logger("moveit_py.pose_goal")
+
+# instantiate MoveItPy instance and get planning component
+panda = MoveItPy(node_name="moveit_py")
+panda_arm = panda.get_planning_component("arm")
+logger.info("MoveItPy instance created")
