2690c4b8251d549eddb69e8632662f54c8556b22
cricel <cricel.design@gmail.com>
2024-10-06 16:39:39 -0400

    update camer aview funs

--- mechlmm_noetic_ws/src/mechlmm_bringup/scripts/camera_view.py
+++ mechlmm_noetic_ws/src/mechlmm_bringup/scripts/camera_view.py
@@ -14,6 +14,7 @@ from pydantic import BaseModel, Field
 import requests
 
 from mechlmm_py import utilities_core, lmm_function_pool
+import function_pool_lmm_declaration
 
 class image_converter:
     def __init__(self):
@@ -22,10 +23,63 @@ class image_converter:
         self.image_sub = rospy.Subscriber("/camera/head/image_raw",Image,self.head_callback)
         self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.base_callback)
 
+        # self.image_sub = rospy.Subscriber("/command",String,self.command_callback)
+        rospy.Timer(rospy.Duration(1), self.timer_callback)
 
-        self.hand_cam = False
-        self.head_cam = False
-        self.base_cam = False
+        self.img_query = False
+
+        self.head_cam = None
+        self.hand_cam = None
+        self.base_cam = None
+
+    def timer_callback(self, msg):
+        self.llm_chat_image()
+
+    def llm_chat_image(self):
+        if(not self.img_query):
+            self.img_query = True
+
+            print("----")
+            if (self.head_cam is None or self.hand_cam is None or self.base_cam is None):
+                return
+
+            url = 'http://192.168.1.134:5001/mechlmm/chat'
+            
+            hand_image_url = utilities_core.opencv_frame_to_base64(self.hand_cam)
+            head_image_url = utilities_core.opencv_frame_to_base64(self.head_cam)
+            base_image_url = utilities_core.opencv_frame_to_base64(self.base_cam)
+
+            query = """
+                    Base on the information and image provided, what are the list of action should do to use end effector to grab the coke can
+
+                    This is a 2 wheel driving robot with a 34 dof arm attach to the top
+                    the image provided in order is:
+                    1. hand camera which attach to the end effector
+                    2. head camera which alway look at the entire robot view from top left angle
+                    3. base camera which mount at the base on the robot
+                """
+
+            data = {
+                'question': query,
+                # 'schema': dict_schema,
+                'tag': 'head_callback',
+                'base_img': [hand_image_url, head_image_url, base_image_url],
+                'tools': [convert_to_openai_function(function_pool_lmm_declaration.arm_end_effector_control),
+                          convert_to_openai_function(function_pool_lmm_declaration.arm_end_effector_rotation_control),
+                          convert_to_openai_function(function_pool_lmm_declaration.trigger_gripper),
+                          convert_to_openai_function(function_pool_lmm_declaration.move_robot)
+                          ]
+            }
+
+            response = requests.post(url, json=data)
+
+            if response.status_code == 200:
+                _result = response.json()
+                print('Success: \n', _result)
+            else:
+                print('Failed:', response.status_code, response.text)
+
+            self.img_query = False
 
     def hand_callback(self,data):
         try:
@@ -33,70 +87,7 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        if(not self.hand_cam):
-            self.hand_cam = True
-
-            print("hand_callback --")
-
-            image_url = utilities_core.opencv_frame_to_base64(cv_image)
-
-            url = 'http://192.168.1.134:5001/mechlmm/chat/image'
-
-            # dict_schema = convert_to_openai_function(ListItems)
-
-            # query = """
-            #         you are a AI on a 2 wheel robot with a single arm attach to it.
-            #         and this a camera on the end effector of the arm
-            #         in order to grab the coke can, what action should be taken
-            #     """
-            
-            data = {
-                'question': """
-                    did you see a coke can in the image
-                """,
-                # 'schema': dict_schema,
-                'tag': 'hand_callback',
-                'base_img': image_url
-            }
-
-            response = requests.post(url, json=data)
-
-            if response.status_code == 200:
-                _result = response.json()
-                print('Success: \n', _result)
-            else:
-                print('Failed:', response.status_code, response.text)
-            
-            print("hand_callback --")
-            self.hand_cam = False
-
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
-
-
-    def llm_chat_image(_frame, _tag):
-        url = 'http://192.168.1.134:5001/mechlmm/chat/image'
-        
-        image_url = utilities_core.opencv_frame_to_base64(_frame)
-
-        data = {
-            'question': """
-                did you see a coke can in the image
-            """,
-            # 'schema': dict_schema,
-            'tag': 'head_callback',
-            'base_img': image_url
-        }
-
-        response = requests.post(url, json=data)
-
-        if response.status_code == 200:
-            _result = response.json()
-            print('Success: \n', _result)
-        else:
-            print('Failed:', response.status_code, response.text)
-        
-
+        self.hand_cam = cv_image
 
     def head_callback(self, data):
         try:
@@ -104,45 +95,7 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        if(not self.hand_cam):
-            self.hand_cam = True
-
-            print("head_callback --")
-
-            image_url = utilities_core.opencv_frame_to_base64(cv_image)
-
-            url = 'http://192.168.1.134:5001/mechlmm/chat/image'
-
-            # dict_schema = convert_to_openai_function(ListItems)
-
-            # query = """
-            #         you are a AI on a 2 wheel robot with a single arm attach to it.
-            #         and this a camera on the end effector of the arm
-            #         in order to grab the coke can, what action should be taken
-            #     """
-            
-            data = {
-                'question': """
-                    did you see a coke can in the image
-                """,
-                # 'schema': dict_schema,
-                'tag': 'head_callback',
-                'base_img': image_url
-            }
-
-            response = requests.post(url, json=data)
-
-            if response.status_code == 200:
-                _result = response.json()
-                print('Success: \n', _result)
-            else:
-                print('Failed:', response.status_code, response.text)
-            
-            print("head_callback --")
-            self.hand_cam = False
-
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
+        self.head_cam = cv_image
     
     def base_callback(self, data):
         try:
@@ -150,50 +103,12 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        if(not self.hand_cam):
-            self.hand_cam = True
-
-            print("base_callback --")
-
-            image_url = utilities_core.opencv_frame_to_base64(cv_image)
-
-            url = 'http://192.168.1.134:5001/mechlmm/chat/image'
-
-            # dict_schema = convert_to_openai_function(ListItems)
-
-            # query = """
-            #         you are a AI on a 2 wheel robot with a single arm attach to it.
-            #         and this a camera on the end effector of the arm
-            #         in order to grab the coke can, what action should be taken
-            #     """
-            
-            data = {
-                'question': """
-                    did you see a coke can in the image
-                """,
-                # 'schema': dict_schema,
-                'tag': 'base_callback',
-                'base_img': image_url
-            }
-
-            response = requests.post(url, json=data)
-
-            if response.status_code == 200:
-                _result = response.json()
-                print('Success: \n', _result)
-            else:
-                print('Failed:', response.status_code, response.text)
-            
-            print("base_callback --")
-            self.hand_cam = False
-
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
+        self.base_cam = cv_image
 
 
 def main(args):
-    ic = image_converter()
     rospy.init_node('image_converter', anonymous=True)
+    ic = image_converter()
     try:
         rospy.spin()
     except KeyboardInterrupt:
