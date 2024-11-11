3b4d8822440847fea8093e1fab63c7b03e8aa33a
cricel <cricel.design@gmail.com>
2024-11-11 17:24:58 -0500

    update git ignore

--- mechlmm_noetic_ws/src/mechlmm_intention/scripts/joint_controller.py
+++ mechlmm_noetic_ws/src/mechlmm_intention/scripts/joint_controller.py
@@ -13,14 +13,15 @@ from intention_core import IntentionCore
 class UnityConnector:
     def __init__(self):
         self.skeleton_joints_sub = rospy.Subscriber("skeleton_joints", Float32MultiArray, self.skeleton_joints_callback)
+        
         self.recording_trigger_sub = rospy.Subscriber("recording_trigger", Bool, self.recording_trigger_callback)
-        self.training_info_sub = rospy.Subscriber("training_info", String, self.training_info_callback)
-
         self.recording_trigger_pub = rospy.Publisher('recording_trigger', Bool, queue_size=10)
-        # self.training_info_pub = rospy.Publisher('training_info', String, queue_size=10)
+
+        self.training_info_sub = rospy.Subscriber("training_info", String, self.training_info_callback)
+        self.training_info_pub = rospy.Publisher('training_info', String, queue_size=10)
 
         self.is_recording = False
-        self.trainning_counter = 1
+        self.trainning_counter = 0
 
         self.training_info_text = []
 
@@ -28,20 +29,28 @@ class UnityConnector:
         # self.it.folder_creation()
 
     def skeleton_joints_callback(self, _msg):
-        if(self.is_recording):
-            my_array = np.array(_msg.data)
+        # if(self.is_recording):
+        #     try:
+        #         my_array = np.array(_msg.data)
+        #         if(my_array.size != 0):
+        #             self.it.add_trainning_data(my_array, 
+        #                                     self.training_info_text[0], 
+        #                                     self.training_info_text[1], 
+        #                                     str(self.trainning_counter))
+                    
+        #             print(self.trainning_counter)
+        #             self.trainning_counter += 1
+        #             if(self.trainning_counter >= 30):
+        #                 self.trainning_counter = 0
+        #                 self.is_recording = False
+        #                 self.recording_trigger_pub.publish(self.is_recording)
+        #     except Exception as e:
+        #         print("An error occurred:", e)
 
-            self.it.add_trainning_data(my_array, 
-                                       self.training_info_text[0], 
-                                       self.training_info_text[1], 
-                                       str(self.trainning_counter))
-            
-
-            self.trainning_counter += 1
-            if(self.trainning_counter >= 30):
-                self.trainning_counter = 1
-                self.is_recording = False
-                self.recording_trigger_pub.publish(self.is_recording)
+        my_array = np.array(_msg.data)
+        result_msg = self.it.live_prediction(my_array)
+        if(result_msg != ""):
+            self.training_info_pub.publish(result_msg)
 
 
     def recording_trigger_callback(self, _msg):
