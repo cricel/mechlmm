fd9917d9876c91a46f39f7a5fa8adc99c48f8590
Xiangxu Lin <cricel.design@gmail.com>
2024-11-12 20:19:02 -0500

    update library

--- mechlmm_unity/Assets/CustomAssets/Scripts/RecordingController.cs
+++ mechlmm_unity/Assets/CustomAssets/Scripts/RecordingController.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
-using Unity.Robotics.ROSTCPConnector;
-using RosMessageTypes.Std;
+// using Unity.Robotics.ROSTCPConnector;
+// using RosMessageTypes.Std;
 using TMPro;
 using System.Collections;
 
 public class RecordingController : MonoBehaviour
 {
     [Header("ROS")]
-    private ROSConnection ros;
+    // private ROSConnection ros;
 
     [SerializeField]
     private string recordingTopicName;
@@ -30,12 +30,12 @@ public class RecordingController : MonoBehaviour
     private TMP_Text countDownText;
     void Start()
     {
-        ros = ROSConnection.GetOrCreateInstance();
-        ros.RegisterPublisher<BoolMsg>(recordingTopicName);
-        ros.RegisterPublisher<StringMsg>(trainingInfoTopicName);
+        // ros = ROSConnection.GetOrCreateInstance();
+        // ros.RegisterPublisher<BoolMsg>(recordingTopicName);
+        // ros.RegisterPublisher<StringMsg>(trainingInfoTopicName);
         
-        ros.Subscribe<BoolMsg>(recordingTopicName, RecordingTriggerCallback);
-        ros.Subscribe<StringMsg>(trainingInfoTopicName, TrainingInfoCallback);
+        // ros.Subscribe<BoolMsg>(recordingTopicName, RecordingTriggerCallback);
+        // ros.Subscribe<StringMsg>(trainingInfoTopicName, TrainingInfoCallback);
     }
 
     // Update is called once per frame
@@ -44,29 +44,29 @@ public class RecordingController : MonoBehaviour
         
     }
 
-    public void RecordingTriggerCallback(BoolMsg _msg){
-        isRecording = _msg.data;
-        if(isRecording){
-            recordingIcon.image.color = Color.green;
-        }
-        else{
-            recordingIcon.image.color = Color.red;
-        }
-    }
+    // public void RecordingTriggerCallback(BoolMsg _msg){
+    //     isRecording = _msg.data;
+    //     if(isRecording){
+    //         recordingIcon.image.color = Color.green;
+    //     }
+    //     else{
+    //         recordingIcon.image.color = Color.red;
+    //     }
+    // }
 
-    public void TrainingInfoCallback(StringMsg _msg){
-        titleText.text = _msg.data;
-    }
+    // public void TrainingInfoCallback(StringMsg _msg){
+    //     titleText.text = _msg.data;
+    // }
 
-    public void PublishRecordingTrigger(){
-        BoolMsg trigger = new BoolMsg(!isRecording);
-        ros.Publish(recordingTopicName, trigger);
+    // public void PublishRecordingTrigger(){
+    //     BoolMsg trigger = new BoolMsg(!isRecording);
+    //     ros.Publish(recordingTopicName, trigger);
 
-        StringMsg actionInfo = new StringMsg(actionName + "," + sequenceNum.ToString());
-        ros.Publish(trainingInfoTopicName, actionInfo);
+    //     StringMsg actionInfo = new StringMsg(actionName + "," + sequenceNum.ToString());
+    //     ros.Publish(trainingInfoTopicName, actionInfo);
 
-        sequenceNum += 1;
-    }
+    //     sequenceNum += 1;
+    // }
 
     public void RecordingReady(){
         StartCoroutine(CountdownCoroutine());
@@ -82,11 +82,11 @@ public class RecordingController : MonoBehaviour
             yield return new WaitForSeconds(1f);
         }
 
-        BoolMsg trigger = new BoolMsg(!isRecording);
-        ros.Publish(recordingTopicName, trigger);
+        // BoolMsg trigger = new BoolMsg(!isRecording);
+        // ros.Publish(recordingTopicName, trigger);
 
-        StringMsg actionInfo = new StringMsg(actionName + "," + sequenceNum.ToString());
-        ros.Publish(trainingInfoTopicName, actionInfo);
+        // StringMsg actionInfo = new StringMsg(actionName + "," + sequenceNum.ToString());
+        // ros.Publish(trainingInfoTopicName, actionInfo);
 
         sequenceNum += 1;
 
